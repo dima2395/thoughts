c1447ce62a46d5f52621341317aec43e094ab462
Gvozdinsky Dmitry <gdima2395@gmail.com>
2017-01-20 16:41:47 +0200

    bday fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.forms import extras
 from .models import Profile
 
 class ProfileForm(forms.ModelForm):
-  birthday = forms.DateField(widget=extras.SelectDateWidget)
+  birthday = forms.DateField(widget=extras.SelectDateWidget(year=[for x in range(1900, 2005)]))
   class Meta:
     model = Profile
     exclude = ['user']
