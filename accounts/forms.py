f802f211f7b1bc0c7d38ead3952573715af53705
Gvozdinsky Dmitry <gdima2395@gmail.com>
2017-01-20 16:51:13 +0200

    bday fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Profile
 
 class ProfileForm(forms.ModelForm):
-  birthday = forms.DateField(widget=forms.TextInput(attrs={'placeholder': 'yyyy-mm-dd ex. 1995-01-30'}))
+  birthday = forms.DateField(widget=forms.TextInput(attrs={'placeholder': 'format: 1995-01-30'}))
   class Meta:
     model = Profile
     exclude = ['user']
