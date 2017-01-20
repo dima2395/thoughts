6d537674b3f1158d8deec1ea25db2d926cb7f1f2
Gvozdinsky Dmitry <gdima2395@gmail.com>
2017-01-20 16:31:34 +0200

    changed birthday widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Profile
 
 class ProfileForm(forms.ModelForm):
+  birthday = forms.DateField(widget=extras.SelectDateWidget)
   class Meta:
     model = Profile
     exclude = ['user']
