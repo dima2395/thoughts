8758980ab22143a4de31dcedbacf898c87a4b2d0
Gvozdinsky Dmitry <gdima2395@gmail.com>
2017-01-29 14:29:00 +0200

    added statistics in profile and some fixes

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django import http
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse, reverse_lazy
 from .forms import ProfileForm
+from django.db.models import Count
 
 
 
@@ -15,13 +16,28 @@ def profile_detail(request, pk):
     try:
       profile = Profile.objects.get(user_id=pk)
       thoughts = Thought.objects.filter(user_id=pk).order_by('-date') #user's thoughts
-      print(profile)
+      statuses = Thought.objects.filter(user_id=pk).values('status').annotate(count=Count('id'))
+      statuses_data = {}
+      statuses_data['labels'] = []
+      statuses_data['values'] = []
+      statuses_data['bgcolors'] = []
+
+      for status in statuses:
+        statuses_data['labels'].append(status['status'].capitalize())
+        statuses_data['values'].append(status['count'])
+        if status['status'] == 'neutral':
+          statuses_data['bgcolors'].append('rgba(230, 230, 230, 0.76)')
+        elif status['status'] == 'positive':
+          statuses_data['bgcolors'].append('rgba(185, 255, 171, 0.76)')
+        elif status['status'] == 'negative':
+          statuses_data['bgcolors'].append('rgba(255, 187, 198, 0.76)')
+
     except Profile.DoesNotExist:
       if request.user.is_authenticated():
         return http.HttpResponseRedirect(reverse('accounts:profile-detail', kwargs={'pk': request.user.pk}))
       else:
         return http.HttpResponseRedirect(reverse_lazy('thoughts:index'))
-    return render(request, 'thoughts/profile.html', {"profile": profile, "thoughts": thoughts})
+    return render(request, 'thoughts/profile.html', {"profile": profile, "thoughts": thoughts, "statuses_data": statuses_data})
 
 
 
